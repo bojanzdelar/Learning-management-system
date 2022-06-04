13ca44e0ad419e759afd0661cc7f9b173aaaff60
Bojan Zdelar <bojan@zdelar.com>
2022-06-04 18:03:20 +0200

    Initialize backend

---
+++ backend/app/src/main/java/rs/ac/uns/app/UniversityOfNoviSadApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityOfNoviSadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityOfNoviSadApplication.class, args);
+	}
+
+}
---
+++ backend/app/src/test/java/rs/ac/uns/app/UniversityOfNoviSadApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityOfNoviSadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
